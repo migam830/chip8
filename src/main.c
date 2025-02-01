4972ba8873d1448e21a75422f70925762c6a5fe2
migam830 <migam830@hotmail.com>
2025-02-01 18:06:26 +0000

    Added memory, stack, timers and some input handling

--- src/main.c
+++ src/main.c
@@ -1,19 +1,8 @@
 #include <SDL3/SDL.h>
+#include <time.h>
+#include <stdio.h>
 
-int main() {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Event event;
-
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer("CHIP-8 emulator", 640, 320, SDL_WINDOW_RESIZABLE, &window, &renderer);
-
-    while (1) {
-        SDL_PollEvent(&event);
-        if (event.type == SDL_EVENT_QUIT) {
-            break;
-        }
-
+void SDLRender(SDL_Renderer *renderer) {
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
 
@@ -35,6 +24,55 @@ int main() {
         }
 
         SDL_RenderPresent(renderer);
+}
+
+int main() {
+    // Memory and stack
+    unsigned char memory[4096] = { 0 };
+    unsigned char stack[16] = { 0 };
+
+
+    // Timers
+    unsigned char delayTimer = 0xFF;
+    unsigned char soundTimer = 0xFF;
+
+    // SDL graphics
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer("CHIP-8 emulator", 640, 320, SDL_WINDOW_RESIZABLE, &window, &renderer);
+
+
+    clock_t clock1 = clock();
+    // SDL event loop and fetch-decode-execute loop
+    while (1) {
+        // Decrement timers if necessary
+        if (delayTimer > 0x00) {
+            clock_t timeElapsed = (clock() - clock1) * 60 / CLOCKS_PER_SEC;
+            int i;
+            for (i = 0; i < timeElapsed; i++) {
+                if (delayTimer <= 0x00) {
+                    break;
+                }
+                delayTimer -= 0x01;
+            }
+            printf("%x\n", delayTimer);
+        }
+
+        SDL_PollEvent(&event);
+        if (event.type == SDL_EVENT_QUIT) {
+            break;
+        }
+        // Keyboard keys (add more when needed)
+        if (event.type == SDL_EVENT_KEY_DOWN) {
+            if (event.key.scancode == SDL_SCANCODE_Q) {
+                printf("Q pressed\n");
+            }
+        }
+
+        SDLRender(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
