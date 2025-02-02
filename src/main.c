147aaf06dc449c5a6309c64d58007cae1bb0f998
migam830 <migam830@hotmail.com>
2025-02-02 01:03:04 +0000

    Added keyboard and temporarily removed timer

--- src/main.c
+++ src/main.c
@@ -26,6 +26,59 @@ void SDLRender(SDL_Renderer *renderer) {
         SDL_RenderPresent(renderer);
 }
 
+void updateKeyboard(int *keyboard, int value, SDL_Scancode scanCode) {
+    switch (scanCode) {
+        case SDL_SCANCODE_1:
+            keyboard[1] = value;
+            break;
+        case SDL_SCANCODE_2:
+            keyboard[2] = value;
+            break;
+        case SDL_SCANCODE_3:
+            keyboard[3] = value;
+            break;
+        case SDL_SCANCODE_4:
+            keyboard[12] = value; // C
+            break;
+        case SDL_SCANCODE_Q:
+            keyboard[4] = value;
+            break;
+        case SDL_SCANCODE_W:
+            keyboard[5] = value;
+            break;
+        case SDL_SCANCODE_E:
+            keyboard[6] = value;
+            break;
+        case SDL_SCANCODE_R:
+            keyboard[13] = value; // D
+            break;
+        case SDL_SCANCODE_A:
+            keyboard[7] = value;
+            break;
+        case SDL_SCANCODE_S:
+            keyboard[8] = value;
+            break;
+        case SDL_SCANCODE_D:
+            keyboard[9] = value;
+            break;
+        case SDL_SCANCODE_F:
+            keyboard[14] = value; // E
+            break;
+        case SDL_SCANCODE_Z:
+            keyboard[10] = value; // A
+            break;
+        case SDL_SCANCODE_X:
+            keyboard[0] = value;
+            break;
+        case SDL_SCANCODE_C:
+            keyboard[11] = value; // B
+            break;
+        case SDL_SCANCODE_V:
+            keyboard[15] = value; // F
+            break;
+    }
+}
+
 int main() {
     // Memory and stack
     unsigned char memory[4096] = { 0 };
@@ -35,6 +88,9 @@ int main() {
     // Timers
     unsigned char delayTimer = 0xFF;
     unsigned char soundTimer = 0xFF;
+
+    // Keyboard keys (index converted to hex is number / letter on key)
+    int keyboard[16] = { 0 };
 
     // SDL graphics
     SDL_Window *window;
@@ -46,32 +102,28 @@ int main() {
 
 
     clock_t clock1 = clock();
+
+    int running = 1;
     // SDL event loop and fetch-decode-execute loop
-    while (1) {
+    while (running != 0) {
+        clock_t timeElapsed = (clock() - clock1) * 60 / CLOCKS_PER_SEC;
         // Decrement timers if necessary
-        if (delayTimer > 0x00) {
-            clock_t timeElapsed = (clock() - clock1) * 60 / CLOCKS_PER_SEC;
-            int i;
-            for (i = 0; i < timeElapsed; i++) {
-                if (delayTimer <= 0x00) {
-                    break;
-                }
-                delayTimer -= 0x01;
-            }
-            printf("%x\n", delayTimer);
-        }
+        // Implement timer functionality later
 
-        SDL_PollEvent(&event);
-        if (event.type == SDL_EVENT_QUIT) {
-            break;
-        }
-        // Keyboard keys (add more when needed)
-        if (event.type == SDL_EVENT_KEY_DOWN) {
-            if (event.key.scancode == SDL_SCANCODE_Q) {
-                printf("Q pressed\n");
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = 0;
+            }
+            // Keyboard keys (add more when needed)
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                updateKeyboard(keyboard, 1, event.key.scancode);
+            }
+            if (event.type == SDL_EVENT_KEY_UP) {
+                updateKeyboard(keyboard, 0, event.key.scancode);
             }
         }
 
+        SDL_Delay(50);
         SDLRender(renderer);
     }
 
