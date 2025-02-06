0635e773ae8fe3154e049751ac10dad49dbe3dab
migam830 <migam830@hotmail.com>
2025-02-06 23:32:37 +0000

    Swapped function positions in header file

--- src/utils.h
+++ src/utils.h
@@ -2,8 +2,8 @@
 #define UTILS_H
 #include <SDL3/SDL.h>
 
+int loadFile(char *fileName, unsigned char *memory);
 void SDLRender(SDL_Renderer *renderer, int *pixels);
 void updateKeyboard(int *keyboard, int value, SDL_Scancode scanCode);
-int loadFile(char *fileName, unsigned char *memory);
 
 #endif
